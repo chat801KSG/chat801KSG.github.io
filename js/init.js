d65350514344f2d05090d0cccfbdc8b3a25b5c26
chat801KSG <87743013+chat801KSG@users.noreply.github.com>
2021-07-20 22:30:01 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// esto inicializa los servicios de firebase utilizando la huella digital unica
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBbTeCNvYKNBYfxzB-igx--LxGYj731zmg",
+    authDomain: "chat801ksg.firebaseapp.com",
+    projectId: "chat801ksg",
+    storageBucket: "chat801ksg.appspot.com",
+    messagingSenderId: "616018555691",
+    appId: "1:616018555691:web:8507f86ce4f0dba148cb6e",
+    measurementId: "G-6QYQ4S0NJK"
+})
